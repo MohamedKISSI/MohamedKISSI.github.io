278d70f93022de3e3c20f247ebf6bf0877387b3f
MohamedKISSI <117038030+MohamedKISSI@users.noreply.github.com>
2025-12-15 12:23:20 +0000

    Deploying to gh-pages from @ MohamedKISSI/MohamedKISSI.github.io@dd9090b5a11c9d05c6f87660deede80a3674ef1d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          description: "Activités d&#39;enseignement et de formation.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
